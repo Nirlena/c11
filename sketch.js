1d20fe0fb35de0044e4bd72ecc960af63d482f57
Nirlena <102743997+Nirlena@users.noreply.github.com>
2022-04-07 11:36:32 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,45 +1,21 @@
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
-
-function preload() {
-  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
-  trex_collided = loadImage("trex_collided.png");
-
-  groundImage = loadImage("ground2.png")
-}
+var weight = [35,38,42,45,43,34,36,41,48,32];
 
 function setup() {
-createCanvas(600, 200);
+  createCanvas(400,400);
+var sum = 0
+for(var i = 0; i < weight.length; i++)
 
-//criar um sprite trex
-trex = createSprite(50,160,20,50);
-trex.addAnimation("running", trex_running);
-trex.scale = 0.5;
-  
-//criar um sprite ground (chão)
-ground = createSprite(200,180,400,20);
-ground.addImage("ground",groundImage);
-ground.x = ground.width /2;
-ground.velocityX = -4;
-  
-invisibleGround = createSprite(200, 190,400, 10)
-invisibleGround.visible=false
+  {
+  sum = sum + weight[i]
+  }
+  var avg = sum/weight.length
+  console.log(avg)
 }
 
-function draw() {
-background(220);
-console.log(trex.y)
-//pular quando a barra de espaço for pressionada
-if (keyDown("space") && trex.y >= 100) {
-  trex.velocityY = -10;
+function draw() 
+{
+  background(30);
 }
 
-trex.velocityY = trex.velocityY + 0.8
+ 
 
-if (ground.x < 0) {
-  ground.x = ground.width / 2;
-}
-
-trex.collide(invisibleGround);
-drawSprites();
-}
